9a45e4fc2c1deecb3aa108b6935a1dc591bb2549
Damien Maillard <damien.maillard@dailymotion.com>
2021-01-01 16:26:48 +0100

    more test on properties

--- test/assert/properties.test.js
+++ test/assert/properties.test.js
@@ -35,6 +35,27 @@ value`,
 }
 
 try {
+  const actual = {
+    a: true,
+    b: true,
+  }
+  const expected = {}
+  assert({ actual, expected })
+} catch (e) {
+  ensureAssertionErrorWithMessage(
+    e,
+    `2 unexpected properties.
+--- unexpected properties ---
+{
+  "a": true,
+  "b": true
+}
+--- at ---
+value`,
+  )
+}
+
+try {
   const actual = {}
   const expected = {
     a: true,
@@ -47,6 +68,27 @@ try {
 --- missing property ---
 {
   "a": true
+}
+--- at ---
+value`,
+  )
+}
+
+try {
+  const actual = {}
+  const expected = {
+    a: true,
+    b: true,
+  }
+  assert({ actual, expected })
+} catch (e) {
+  ensureAssertionErrorWithMessage(
+    e,
+    `2 missing properties.
+--- missing properties ---
+{
+  "a": true,
+  "b": true
 }
 --- at ---
 value`,
@@ -78,6 +120,91 @@ try {
 {
   "d": true,
   "e": true
+}
+--- at ---
+value`,
+  )
+}
+
+try {
+  const actual = {
+    a: true,
+    d: true,
+    e: true,
+  }
+  const expected = {
+    a: true,
+    b: true,
+  }
+  assert({ actual, expected })
+} catch (e) {
+  ensureAssertionErrorWithMessage(
+    e,
+    `1 missing property and 2 unexpected properties.
+--- missing property ---
+{
+  "b": true
+}
+--- unexpected properties ---
+{
+  "d": true,
+  "e": true
+}
+--- at ---
+value`,
+  )
+}
+
+try {
+  const actual = {
+    a: true,
+    d: true,
+  }
+  const expected = {
+    a: true,
+    b: true,
+    c: true,
+  }
+  assert({ actual, expected })
+} catch (e) {
+  ensureAssertionErrorWithMessage(
+    e,
+    `2 missing properties and 1 unexpected property.
+--- missing properties ---
+{
+  "b": true,
+  "c": true
+}
+--- unexpected property ---
+{
+  "d": true
+}
+--- at ---
+value`,
+  )
+}
+
+try {
+  const actual = {
+    a: true,
+    d: true,
+  }
+  const expected = {
+    a: true,
+    b: true,
+  }
+  assert({ actual, expected })
+} catch (e) {
+  ensureAssertionErrorWithMessage(
+    e,
+    `1 missing property and 1 unexpected property.
+--- missing property ---
+{
+  "b": true
+}
+--- unexpected property ---
+{
+  "d": true
 }
 --- at ---
 value`,

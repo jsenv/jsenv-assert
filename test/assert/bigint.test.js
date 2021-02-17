88cadcc1b73c48d55af66c836bec94709379dc35
Damien Maillard <damien.maillard@dailymotion.com>
2021-02-17 16:24:23 +0100

    handle big int comparison and failure

---
+++ test/assert/bigint.test.js
@@ -0,0 +1,28 @@
+/* globals BigInt */
+/* eslint-disable no-eval */
+import { assert } from "@jsenv/assert"
+import { ensureAssertionErrorWithMessage } from "@jsenv/assert/test/ensureAssertionErrorWithMessage.js"
+
+{
+  const actual = BigInt(1)
+  const expected = BigInt(1)
+  assert({ actual, expected })
+}
+
+try {
+  const actual = BigInt(1)
+  const expected = BigInt(2)
+  assert({ actual, expected })
+  throw new Error("should throw")
+} catch (error) {
+  ensureAssertionErrorWithMessage(
+    error,
+    `unequal values.
+--- found ---
+1n
+--- expected ---
+2n
+--- at ---
+value`,
+  )
+}
